e19c76c9e924d4c50ef18b138b83accb95b2dd02
Jatin Goyal <goyaljatin344@gmail.com>
2021-11-30 21:06:04 +0530

    input name completely working

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,12 +1,54 @@
+import { useState } from "react";
 const SimpleInput = (props) => {
+  const [enteredName, setEnteredName] = useState("");
+  const [enteredNameTouched, setEnteredNameTouched] = useState(false);
+
+  // const checkValidity=(inputValue)=>{
+  //   return inputValue.trim() !== "";
+  // }
+  const enteredNameIsValid = enteredName.trim() !== "";
+  const nameInputIsInvalid = enteredNameTouched && !enteredNameIsValid;
+  let formIsValid = false;
+  if (enteredNameIsValid) {
+    formIsValid=true;
+  }
+  const formSubmitHandler = (event) => {
+    setEnteredNameTouched(true);
+    event.preventDefault();
+    if (!enteredNameIsValid) {
+      return; //basic validation
+    }
+    console.log("name is: ", enteredName); //imp work
+    setEnteredName("");
+    setEnteredNameTouched(false);
+  };
+
+  const nameInputBlurHandler = () => {
+    setEnteredNameTouched(true);
+  };
+  const nameInputChangeHandler = (event) => {
+    setEnteredName(event.target.value);
+  };
+  const nameInputClasses = nameInputIsInvalid
+    ? "form-control invalid"
+    : "form-control";
   return (
-    <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+    <form onSubmit={formSubmitHandler}>
+      <div className={nameInputClasses}>
+        <label htmlFor="name">Your Name</label>
+        <input
+          value={enteredName}
+          onChange={nameInputChangeHandler}
+          onBlur={nameInputBlurHandler}
+          type="text"
+          id="name"
+        />
+        {nameInputIsInvalid && (
+          <p className="error-text">Name cannot be empty.</p>
+        )}
       </div>
       <div className="form-actions">
-        <button>Submit</button>
+        <button disabled={!formIsValid}>Submit</button>
       </div>
     </form>
   );
---
+++ src/customHooks/useInputValidity.js
@@ -0,0 +1,8 @@
+import React,{useState} from 'react';
+ const useInputVal = ()=>{
+    const [enteredInput, setEnteredName] = useState("");
+    const [enteredInputTouched, setEnteredNameTouched] = useState(false);
+    
+ }
+
+ export default useInputVal;
